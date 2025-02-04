0566134ddcdf9d152329bd3259e093d9b1feba7d
Max Rohde <1448524+mxro@users.noreply.github.com>
2025-02-04 15:34:47 +1100

    Preparing API project

--- packages/api/infra/aws/api_gateway.tf
+++ packages/api/infra/aws/api_gateway.tf
@@ -5,6 +5,14 @@ resource "aws_apigatewayv2_api" "api" {
   name        = "lambda-api-gateway-${random_id.id.hex}"
   description = "API for Goldstack lambda deployment"
 	protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_credentials = true
+    allow_headers = ["Content-Type","Accept","x-csrf-token"]
+    allow_methods = ["GET","OPTIONS","POST","PUT","DELETE","PATCH"]
+    allow_origins = [var.cors]
+  }
+
 }
 
 # see https://github.com/terraform-aws-modules/terraform-aws-apigateway-v2/blob/v1.5.1/main.tf#L137
