b0f0d1d033e76f814955b0020fe007f62b6cb7c2
Max Rohde <1448524+mxro@users.noreply.github.com>
2025-04-03 09:21:51 +1100

    Fixing google analytics

--- packages/ui/pages/_app.tsx
+++ packages/ui/pages/_app.tsx
@@ -6,7 +6,19 @@ import './globals.css';
 const App = ({ Component, pageProps }): JSX.Element => {
   process.env.GOLDSTACK_DEPLOYMENT =
     process.env.NEXT_PUBLIC_GOLDSTACK_DEPLOYMENT;
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WZ7HST3B"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
--- packages/ui/pages/_document.tsx
+++ packages/ui/pages/_document.tsx
@@ -6,15 +6,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script src="https://www.googletagmanager.com/gtag/js?id=G-SRW19ZZQLW"></script>
           <script
-            content={`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-SRW19ZZQLW');
-`}
+            dangerouslySetInnerHTML={{
+              __html: `<!-- Google Tag Manager -->
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WZ7HST3B');</script>
+<!-- End Google Tag Manager --> `,
+            }}
           ></script>
           <link
             rel="apple-touch-icon"
