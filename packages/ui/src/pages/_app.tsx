9bc50fadd2ed2ccd3545882e6879c8ff7a1b0390
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-09-27 16:46:18 +0700

    Restoring further original config and pages

--- packages/ui/src/pages/_app.tsx
+++ packages/ui/src/pages/_app.tsx
@@ -1,10 +1,24 @@
-// biome-ignore lint/correctness/noUnusedImports: React
+/* eslint-disable react/prop-types */
 import React from 'react';
+
 import './globals.css';
 
 const App = ({ Component, pageProps }): JSX.Element => {
-  process.env.GOLDSTACK_DEPLOYMENT = process.env.NEXT_PUBLIC_GOLDSTACK_DEPLOYMENT;
-  return <Component {...pageProps} />;
+  process.env.GOLDSTACK_DEPLOYMENT =
+    process.env.NEXT_PUBLIC_GOLDSTACK_DEPLOYMENT;
+  return (
+    <>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-WZ7HST3B"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default App;
