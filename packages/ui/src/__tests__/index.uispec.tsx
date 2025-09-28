a56b085414e948e7b9a8d39b675eb4262e1c0873
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-09-28 19:08:00 +0700

    fixing warning

--- packages/ui/src/__tests__/index.uispec.tsx
+++ packages/ui/src/__tests__/index.uispec.tsx
@@ -1,8 +1,10 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import '@testing-library/jest-dom';
+import assert from 'node:assert';
 import Homepage from '../pages/index';
 
 test('Check App component render', () => {
   const res = render(<Homepage />);
+  assert(res !== null);
 });
