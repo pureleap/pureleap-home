3e865c95e2b8f592d6d165c36bfcb0fb62acd5b0
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-09-30 10:08:29 +0700

    w

--- packages/ui/src/components/GiftsSection.tsx
+++ packages/ui/src/components/GiftsSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import commonplaceBookImage from '../img/gifts/commonplace-book.png';
 import riverNotABucketImage from '../img/gifts/river-not-a-bucket.png';
+import GiftBox from './gift-box-256x256.png';
 
 interface GiftsSectionProps {
   className?: string;
@@ -35,7 +36,7 @@ export const GiftsSection: React.FC<GiftsSectionProps> = ({ className = '' }) =>
         {gifts.map((gift, index) => (
           <div
             key={index}
-            className="flex flex-col text-center max-w-64 border rounded-lg border-gray-200 group group-hover:bg-gray-100 hover:bg-gray-200 cursor-pointer"
+            className="flex flex-col text-center max-w-64 group group-hover:bg-gray-100 hover:bg-gray-200 cursor-pointer"
           >
             <div className="flex-1 p-4">
               <div className="mb-6">
@@ -49,12 +50,14 @@ export const GiftsSection: React.FC<GiftsSectionProps> = ({ className = '' }) =>
               <p className="text-base font-sans text-gray-600 mb-4">{gift.subtitle}</p>
               <p className="text-base font-sans text-gray-800">{gift.description}</p>
             </div>
-            <div className="flex justify-between items-center border-t border-gray-200 h-12 max-h-12">
-              <div className="flex items-center justify-center border-r border-gray-200 h-12 w-12 ">
-                <span className="text-base font-sans">$0</span>
+            <div className="flex justify-between items-center border rounded-lg border-gray-200 h-8 max-h-8 group-hover:bg-gray-300 mr-8 ml-8">
+              <div className="flex items-center justify-center border-r border-gray-200 h-8 w-8 ">
+                <span className="text-base font-sans">
+                  <img src={GiftBox} className="h-10 w-10" alt="Gift box"></img>
+                </span>
               </div>
               <div className="flex items-center justify-center flex-1">
-                <a href={gift.link} className="btn pt-1 pb-1 pr-2 pl-2 group-hover:bg-gray-300">
+                <a href={gift.link} className="btn pt-1 pb-1 pr-2 pl-2">
                   learn more
                 </a>
               </div>
