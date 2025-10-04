c20821c8a7d78c5c36d20c4c514c504994ae6e1e
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-10-04 17:33:55 +0700

    w

--- packages/ui/src/components/gift/WhatWeHaveMadeSection.tsx
+++ packages/ui/src/components/gift/WhatWeHaveMadeSection.tsx
@@ -1,4 +1,5 @@
 import book from './commonplace-book.png';
+import instructions from './handwritten-instructions.png';
 import study from './Vittore_carpaccio.jpg';
 export function WhatWeHaveMadeSection() {
   return (
@@ -16,7 +17,7 @@ export function WhatWeHaveMadeSection() {
         </div>
         <div>
           <img
-            className="w-96"
+            className="w-96 rounded-lg shadow-xl shadow-blue-gray-900/50"
             src={study}
             alt="Oil painting: Vittore carpaccio, visione di sant'agostino 01"
           ></img>
@@ -25,16 +26,36 @@ export function WhatWeHaveMadeSection() {
       <div className="mt-48 flex justify-center flex-col items-center">
         <div className="text-xl max-w-lg">
           <p className="max-w-lg text-center">
-            We've made a <b>Notion template</b> that allows you to create your own digital
-            Commonplace book.
+            We made a <b>Notion template</b> that allows you to create your own digital Commonplace
+            book.
           </p>
         </div>
         <div>
           <img
             src={book}
-            className="mt-8 max-w-3xl"
+            className="mt-8 max-w-3xl rounded-lg shadow-xl shadow-blue-gray-900/50"
             alt="Screenshot of Notion Commonplace book"
           ></img>
+        </div>
+      </div>
+      <div className="mt-48 flex justify-center flex-col items-center">
+        <div className="text-xl max-w-lg">
+          <p>What's included:</p>
+        </div>
+        <div className="mt-8 flex flex-col items-center justify-center">
+          <div className="flex flex-row items-center justify-evenly">
+            <div>
+              {/* https://new.express.adobe.com/id/urn:aaid:sc:AP:79a9ebcb-18d1-4680-be7b-295e9e8da54f?invite=true&accept=true?preload=sharesheet */}
+              <img
+                src={instructions}
+                alt="handwritten instructions"
+                className="max-w-64 rounded-lg shadow-xl shadow-blue-gray-900/50"
+              ></img>
+            </div>
+            <div className="mx-16 max-w-md">
+              Handwritten notes to help you <em>slow down</em> and create something meaningful.
+            </div>
+          </div>
         </div>
       </div>
     </section>
