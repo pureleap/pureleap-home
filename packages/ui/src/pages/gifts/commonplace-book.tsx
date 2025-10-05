09c15987acec0870d7a3d274f9a97ba6de0e8e71
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-10-05 16:09:34 +0700

    Help me make @/packages/ui/src/components/gifts/commonplace-book/WhyGiveItATrySection.tsx  responsive
    on smaller screens i want a fixed width of 18 for the images and give them a left padding of 2, and the text for the items a right padding of 2

--- packages/ui/src/pages/gifts/commonplace-book.tsx
+++ packages/ui/src/pages/gifts/commonplace-book.tsx
@@ -70,15 +70,15 @@ const GiftPage: React.FC = () => {
       <WhatWeHaveMadeSection></WhatWeHaveMadeSection>
       <WhyGiveItATrySection></WhyGiveItATrySection>
       <section className="pt-32 bg-white">
-        <div className="flex flex-row justify-center">
-          <div className="flex items-center font-sans text-center text-xl max-w-64">
+        <div className="flex flex-col sm:flex-row justify-center">
+          <div className="flex items-center justify-center font-sans text-center text-xl max-w-64 px-2 sm:px-0 self-center">
             <div>
               <p className="mb-4">
                 Explore the template on Notion and we hope you'll give it a try!
               </p>
             </div>
           </div>
-          <div className="pl-16">
+          <div className="mt-4 sm:mt-0 sm:pl-16 flex justify-center">
             <GiftCTA link={gift.ctaLink} />
           </div>
         </div>
