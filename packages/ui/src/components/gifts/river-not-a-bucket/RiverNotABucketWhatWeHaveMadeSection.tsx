019e74a56a966b6e30931dc9c7201a4a59c10f37
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-10-06 12:24:16 +0700

    adding section

--- packages/ui/src/components/gifts/river-not-a-bucket/RiverNotABucketWhatWeHaveMadeSection.tsx
+++ packages/ui/src/components/gifts/river-not-a-bucket/RiverNotABucketWhatWeHaveMadeSection.tsx
@@ -8,34 +8,34 @@ export function RiverNotABucketWhatWeHaveMadeSection() {
       <div className="flex flex-col md:flex-row justify-center md:justify-evenly items-center">
         <div className="flex items-center font-sans text-center text-xl max-w-96 mb-8 md:mb-0">
           <div>
-            <p className="mb-4">We treat what we collect not as 'to dos' for later.</p>
-            <p>
-              Why don't we instead see them like a cherished river of things; some of which we may
-              pick up later, and some we left drift past.
+            <p className="mb-4">
+              We treat what we collect as <b>tasks</b> for later.
+            </p>
+            <p className="mb-4">
+              Why not see them as a <b>river of things</b> — some we'll pick up, others we'll let
+              drift past?
+            </p>
+            <p className="mb-4">·</p>
+            <p className="mb-4">
+              Our <b>Notion template</b> helps put this idea into practice!
+            </p>
+            <p className="mt-16 text-sm text-gray">
+              Inspired by{' '}
+              <a
+                href="https://www.goodreads.com/review/show/7707610951"
+                target="_blank"
+                rel="noopener"
+                className="underline"
+              >
+                Meditations for Mortals
+              </a>{' '}
+              by Oliver Burkeman.
             </p>
           </div>
         </div>
         <div className="mt-8 md:mt-0">
-          <img
-            className="w-full md:w-96 rounded-lg shadow-xl shadow-blue-gray-900/50"
-            src={man}
-            alt="Man sitting by the riverside"
-          ></img>
+          <img className="w-64 md:w-64" src={man} alt="Man sitting by the riverside"></img>
         </div>
-      </div>
-      <div className="mt-16 flex justify-center flex-col items-center">
-        <div className="text-xl max-w-lg">
-          <p className="max-w-lg text-center">
-            We've made a <b>Notion template</b> so you can create your own digital Commonplace book.
-          </p>
-        </div>
-        {/* <div>
-          <img
-            src={book}
-            className="mt-8 max-w-3xl rounded-lg shadow-xl shadow-blue-gray-900/50"
-            alt="Screenshot of Notion Commonplace book"
-          ></img>
-        </div> */}
       </div>
     </section>
   );
