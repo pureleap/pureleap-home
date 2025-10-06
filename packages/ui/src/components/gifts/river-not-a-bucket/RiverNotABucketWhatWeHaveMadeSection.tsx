ce8a0c8ef22add997cbf72037d80637a22bd6715
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-10-06 12:47:38 +0700

    improving gift page

--- packages/ui/src/components/gifts/river-not-a-bucket/RiverNotABucketWhatWeHaveMadeSection.tsx
+++ packages/ui/src/components/gifts/river-not-a-bucket/RiverNotABucketWhatWeHaveMadeSection.tsx
@@ -5,22 +5,25 @@ export function RiverNotABucketWhatWeHaveMadeSection() {
   return (
     <section className="pt-32 bg-white" id="what-we-have-made">
       <SectionHeading>What we've made for you</SectionHeading>
-      <div className="flex flex-col md:flex-row justify-center md:justify-evenly items-center">
-        <div className="flex items-center font-sans text-center text-xl max-w-96 mb-8 md:mb-0">
+      <div className="flex flex-col md:flex-row justify-center md:justify-center items-center">
+        <div className="flex items-center font-sans text-center text-xl max-w-96 pr-8">
           <div>
             <p className="mb-4">
-              We treat what we collect as <b>tasks</b> for later.
-            </p>
-            <p className="mb-4">
-              Why not see them as a <b>river of things</b> — some we'll pick up, others we'll let
-              drift past?
+              A Notion Template based on an idea<span className="align-super">*</span> by Oliver
+              Burkeman:
             </p>
             <p className="mb-4">·</p>
             <p className="mb-4">
-              Our <b>Notion template</b> helps put this idea into practice!
+              Don't treat what you collect as <b>tasks</b> for later.
             </p>
+            <p className="mb-4">
+              Treat it as flotsam on a <b>river</b>: pick up what calls to you, and let the rest{' '}
+              <em>drift past</em>.
+            </p>
+            <p className="mb-4">·</p>
+            <p></p>
             <p className="mt-16 text-sm text-gray">
-              Inspired by{' '}
+              * From the book{' '}
               <a
                 href="https://www.goodreads.com/review/show/7707610951"
                 target="_blank"
@@ -29,12 +32,15 @@ export function RiverNotABucketWhatWeHaveMadeSection() {
               >
                 Meditations for Mortals
               </a>{' '}
-              by Oliver Burkeman.
             </p>
           </div>
         </div>
-        <div className="mt-8 md:mt-0">
-          <img className="w-64 md:w-64" src={man} alt="Man sitting by the riverside"></img>
+        <div className="pl-8">
+          <img
+            className="hidden md:block w-64 md:w-64"
+            src={man}
+            alt="Man sitting by the riverside"
+          ></img>
         </div>
       </div>
     </section>
