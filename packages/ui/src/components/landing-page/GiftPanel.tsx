e01a8e7dc6cceff2d315ac5127655b32a97a07ed
Max Rohde <{ID}+{username}@users.noreply.github.com>
2025-10-13 13:53:58 +0700

    w

--- packages/ui/src/components/landing-page/GiftPanel.tsx
+++ packages/ui/src/components/landing-page/GiftPanel.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import artSupplies from './art-supplies-background-pattern.jpeg';
+import craftingToolsBackground from './crafting-tools-background-wide.jpeg';
+import gentle from './gentle-background-pattern.jpg';
 
 interface GiftPanelProps {
   children: React.ReactNode;
@@ -6,5 +9,18 @@ interface GiftPanelProps {
 }
 
 export const GiftPanel: React.FC<GiftPanelProps> = ({ children, className = '' }) => {
-  return <div className={`py-8 bg-purple-900 ${className}`}>{children}</div>;
+  return (
+    <div className={`py-16 relative ${className}`}>
+      <div
+        className="absolute inset-0"
+        style={{
+          backgroundImage: `url(${gentle})`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+        }}
+      />
+      <div className="relative z-10">{children}</div>
+    </div>
+  );
 };
